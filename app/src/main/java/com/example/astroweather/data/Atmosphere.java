40ab15ba08757e0aeec2f160f82a8e50663dcd90
Amadeusz Janiak <amadeusz.janiak96@gmail.com>
2018-06-11 11:58:17 +0200

    +add visibility variable

--- app/src/main/java/com/example/astroweather/data/Atmosphere.java
+++ app/src/main/java/com/example/astroweather/data/Atmosphere.java
@@ -6,11 +6,13 @@ public class Atmosphere implements JSONPopulator {
 
     private String pressure;
     private String humidity;
+    private String visibility;
 
     @Override
     public void populate(JSONObject data) {
         this.pressure = data.optString("pressure");
         this.humidity = data.optString("humidity");
+        this.visibility = data.optString("visibility");
     }
 
     public String getPressure() {
@@ -19,5 +21,9 @@ public class Atmosphere implements JSONPopulator {
 
     public String getHumidity() {
         return humidity;
+    }
+
+    public String getVisibility() {
+        return visibility;
     }
 }
