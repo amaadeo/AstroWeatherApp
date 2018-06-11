ca5aee5e69bff869f0a6e42644806c86c1d4d237
Amadeusz Janiak <amadeusz.janiak96@gmail.com>
2018-06-11 11:54:18 +0200

    +refactor Item class

--- app/src/main/java/com/example/astroweather/data/Item.java
+++ app/src/main/java/com/example/astroweather/data/Item.java
@@ -1,18 +1,43 @@
 package com.example.astroweather.data;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Item implements JSONPopulator {
 
+    private static final int NUMBER_OF_DAYS = 5;
     private Condition condition;
+    private Forecast[] forecast;
+    private JSONArray jsonArray;
 
     @Override
     public void populate(JSONObject data) {
         this.condition = new Condition();
         this.condition.populate(data.optJSONObject("condition"));
+
+        this.jsonArray = data.optJSONArray("forecast");
+        this.forecast = new Forecast[NUMBER_OF_DAYS];
+        for (int i = 1; i < NUMBER_OF_DAYS + 1; i++) {
+            try {
+                forecast[i] = new Forecast();
+                forecast[i].populate(jsonArray.getJSONObject(i));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 
     public Condition getCondition() {
         return condition;
+    }
+
+    public Forecast getForecast(int day) {
+        return forecast[day];
+    }
+
+    public JSONArray getJsonArray() {
+        return jsonArray;
     }
 }
