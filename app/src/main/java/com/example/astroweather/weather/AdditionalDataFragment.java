c40bf207d4214d1cc38e5cefc7502f94a937f77c
Amadeusz Janiak <amadeusz.janiak96@gmail.com>
2018-06-12 12:22:33 +0200

    +add unit changing

--- app/src/main/java/com/example/astroweather/weather/AdditionalDataFragment.java
+++ app/src/main/java/com/example/astroweather/weather/AdditionalDataFragment.java
@@ -14,11 +14,15 @@ import java.util.Objects;
 
 public class AdditionalDataFragment extends Fragment {
 
+    public static final double SPEED_IN_MPH = 1.61;
     private TextView windForceText;
     private TextView windDirectionText;
     private TextView humidityText;
     private TextView visibilityText;
     private SharedPreferences sharedPreferences;
+    private String speed;
+    private String speedUnit;
+    private String windSpeed;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,7 +37,7 @@ public class AdditionalDataFragment extends Fragment {
     }
 
     private void setAdditionalWeatherInfo() {
-        windForceText.setText(String.format("Wind speed: %s mph", sharedPreferences.getString("wind_speed", "NULL")));
+        windForceText.setText(String.format("Wind speed: %s %s", changeUnit(sharedPreferences.getString("wind_speed", "NULL")), speedUnit));
         windDirectionText.setText(String.format("Direction: %s", sharedPreferences.getString("wind_direction", "NULL")));
         humidityText.setText(String.format("Humidity: %s %%", sharedPreferences.getString("humidity", "NULL")));
         visibilityText.setText(String.format("Visibility: %s %%", sharedPreferences.getString("visibility", "NULL")));
@@ -45,5 +49,16 @@ public class AdditionalDataFragment extends Fragment {
         humidityText = rootView.findViewById(R.id.humidityText);
         visibilityText = rootView.findViewById(R.id.visibilityText);
         sharedPreferences = Objects.requireNonNull(getContext()).getSharedPreferences("weather.xml", 0);
+        speed = sharedPreferences.getString("temperature_unit", "NULL");
+    }
+
+    private String changeUnit(String windSpeed) {
+        if(speed.equals("0") || speed.equals("NULL")) {
+            speedUnit = "mph";
+        } else {
+            speedUnit = "km/h";
+            windSpeed = String.valueOf(Double.parseDouble(windSpeed) * SPEED_IN_MPH);
+        }
+        return windSpeed;
     }
 }
