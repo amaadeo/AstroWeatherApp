c3240b0c331faf609576cf1d1fc26eb7e0df6544
Amadeusz Janiak <amadeusz.janiak96@gmail.com>
2018-06-12 11:22:22 +0200

    +AdditionalData refactored

--- app/src/main/java/com/example/astroweather/weather/AdditionalDataFragment.java
+++ app/src/main/java/com/example/astroweather/weather/AdditionalDataFragment.java
@@ -27,16 +27,23 @@ public class AdditionalDataFragment extends Fragment {
                 R.layout.fragment_additional_data, container, false);
 
 
+        initElements(rootView);
+        setAdditionalWeatherInfo();
+        return rootView;
+    }
+
+    private void setAdditionalWeatherInfo() {
+        windForceText.setText(String.format("Wind speed: %s mph", sharedPreferences.getString("wind_speed", "NULL")));
+        windDirectionText.setText(String.format("Direction: %s", sharedPreferences.getString("wind_direction", "NULL")));
+        humidityText.setText(String.format("Humidity: %s %%", sharedPreferences.getString("humidity", "NULL")));
+        visibilityText.setText(String.format("Visibility: %s %%", sharedPreferences.getString("visibility", "NULL")));
+    }
+
+    private void initElements(ViewGroup rootView) {
         windForceText = rootView.findViewById(R.id.windForceText);
         windDirectionText = rootView.findViewById(R.id.windDirectionText);
         humidityText = rootView.findViewById(R.id.humidityText);
         visibilityText = rootView.findViewById(R.id.visibilityText);
         sharedPreferences = Objects.requireNonNull(getContext()).getSharedPreferences("weather.xml", 0);
-
-        windForceText.setText("Wind speed: " + sharedPreferences.getString("wind_speed", "NULL") + " mph");
-        windDirectionText.setText("Direction: " + sharedPreferences.getString("wind_direction", "NULL"));
-        humidityText.setText("Humidity: " + sharedPreferences.getString("humidity", "NULL") + " %");
-        visibilityText.setText("Visibility: " + sharedPreferences.getString("visibility", "NULL") + " %");
-        return rootView;
     }
 }
